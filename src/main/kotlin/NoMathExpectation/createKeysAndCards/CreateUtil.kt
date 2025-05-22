f7ea19ee68f2b66c159c49886df366eacb295d11
NoMathExpectation <85624722+NoMathExpectation@users.noreply.github.com>
2025-05-23 00:08:29 +0800

    Added goggle tooltip for guardian chamber.

--- src/main/kotlin/NoMathExpectation/createKeysAndCards/CreateUtil.kt
+++ src/main/kotlin/NoMathExpectation/createKeysAndCards/CreateUtil.kt
@@ -2,6 +2,8 @@ package NoMathExpectation.createKeysAndCards
 
 import com.simibubi.create.api.stress.BlockStressValues
 import com.tterrag.registrate.builders.BlockBuilder
+import net.createmod.catnip.lang.LangBuilder
+import net.createmod.catnip.lang.LangNumberFormat
 import net.createmod.catnip.registry.RegisteredObjectsHelper
 import net.minecraft.resources.ResourceLocation
 import net.minecraft.world.level.block.Block
@@ -36,3 +38,7 @@ internal fun <B : Block, P> BlockBuilder<B, P>.setCapacity(supplier: () -> Doubl
 }
 
 internal fun <B : Block, P> BlockBuilder<B, P>.setCapacity(value: Double = 0.0) = setCapacity { value }
+
+val modLangBuilder get() = LangBuilder(modId)
+
+fun LangBuilder.number(value: Number): LangBuilder = text(LangNumberFormat.format(value.toDouble()))
--- src/main/kotlin/NoMathExpectation/createKeysAndCards/block/guardianChamber/GuardianChamberBlockEntity.kt
+++ src/main/kotlin/NoMathExpectation/createKeysAndCards/block/guardianChamber/GuardianChamberBlockEntity.kt
@@ -1,7 +1,11 @@
 package NoMathExpectation.createKeysAndCards.block.guardianChamber
 
+import NoMathExpectation.createKeysAndCards.modLangBuilder
+import NoMathExpectation.createKeysAndCards.number
 import com.simibubi.create.content.kinetics.base.KineticBlockEntity
+import net.minecraft.ChatFormatting
 import net.minecraft.core.BlockPos
+import net.minecraft.network.chat.Component
 import net.minecraft.world.level.block.entity.BlockEntityType
 import net.minecraft.world.level.block.state.BlockState
 
@@ -9,5 +13,19 @@ class GuardianChamberBlockEntity(
     type: BlockEntityType<*>,
     pos: BlockPos,
     state: BlockState
-): KineticBlockEntity(type, pos, state) {
+) : KineticBlockEntity(type, pos, state) {
+    val effectRange get() = speed
+
+    override fun addToGoggleTooltip(tooltip: MutableList<Component>, isPlayerSneaking: Boolean): Boolean {
+        super.addToGoggleTooltip(tooltip, isPlayerSneaking)
+
+        modLangBuilder.translate("guardian_chamber.tooltip.range")
+            .style(ChatFormatting.GRAY)
+            .forGoggles(tooltip)
+        modLangBuilder.number(effectRange)
+            .style(ChatFormatting.AQUA)
+            .forGoggles(tooltip, 1)
+
+        return true
+    }
 }
